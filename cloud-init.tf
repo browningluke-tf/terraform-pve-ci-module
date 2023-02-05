91ff327fbe706885ef2d243970d39d531e4995be
Luke Browning <browningluke01@gmail.com>
2023-02-04 19:39:46 -0800

    Extract local path to variable

--- cloud-init.tf
+++ cloud-init.tf
@@ -1,6 +1,9 @@
 locals {
   ci_file_user_filename = "${var.vmid}_user.yml"
 
+  # Local storage locations
+  ci_file_local_path_user = "${var.ci_conf_path}/user.cfg"
+
   # Remote storage locations
   ci_file_storage            = "ci"
   ci_file_relative_path_user = "snippets/${local.ci_file_user_filename}"
@@ -8,7 +11,7 @@ locals {
 
 
 resource "local_file" "cloud_init_user_data_file" {
-  content = templatefile("${var.ci_conf_path}/user.cfg", {
+  content = templatefile(local.ci_file_local_path_user, {
     username    = var.username
     defaultpass = var.default_password
     pubkey      = file(pathexpand(var.pubkey_path))
