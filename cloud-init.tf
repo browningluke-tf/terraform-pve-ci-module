11d167fddf133d9505780c5c1c4c35a534b80d3c
Luke Browning <browningluke01@gmail.com>
2023-02-15 21:34:42 -0800

    Use SSH SCP with private key

--- cloud-init.tf
+++ cloud-init.tf
@@ -32,14 +32,22 @@ resource "null_resource" "cloud_init_config_files" {
 
   provisioner "file" {
     source      = local_file.cloud_init_user_data_file.filename
-    destination = "/var/lib/${local.ci_file_storage}/${local.ci_file_relative_path_user}"
+    destination = "/${local.ci_file_user_filename}" # Non-standard path. Read note below
   }
 
+  # Connection REQUIRES the following line in ~/.ssh/authorized_keys:
+  # command="scp -t /path/to/ci/snippets",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ssh-ed25519 <key>
+  # which will prevent the key from shell access & restrict SCP destination.
+  # In /etc/ssh/sshd_config, the line:
+  # Subsystem sftp internal-sftp
+  # is commented out, to restrict sftp access (forces scp to use custom OpenSSL protocol).
+  # NOTE: this only works with OpenSSH version <9.0 (since in >=9.0, scp uses sftp as backend)
+  # TF `provisioner file` runs ssh + scp (remote), thus using `ForceCommand internal-stfp` does not work with TF.
   connection {
-    type     = "ssh"
-    host     = var.pve_host
-    user     = var.pve_user
-    password = var.pve_password
-    agent    = false
+    type        = "ssh"
+    host        = var.pve_host
+    user        = var.pve_user
+    private_key = var.pve_private_key
+    agent       = false
   }
 }
