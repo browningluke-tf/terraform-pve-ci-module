c07e23fb6a403fc8ef09afc561d0cedd54d9be81
Luke Browning <browningluke01@gmail.com>
2024-07-17 14:05:47 -0700

    Replace local_file with terraform_data

--- cloud-init.tf
+++ cloud-init.tf
@@ -1,17 +1,13 @@
 locals {
   ci_file_user_filename = "${var.vmid}_user.yml"
 
-  # Local storage locations
-  ci_file_local_path_user = "${var.ci_conf_path}/user.cfg"
-
   # Remote storage locations
   ci_file_storage            = "ci"
   ci_file_relative_path_user = "snippets/${local.ci_file_user_filename}"
 }
 
-
-resource "local_file" "cloud_init_user_data_file" {
-  content = templatefile(local.ci_file_local_path_user, {
+locals {
+  user_file_data = templatefile(var.ci_user_path, {
     username    = var.username
     defaultpass = var.default_password
     pubkey      = file(pathexpand(var.pubkey_path))
@@ -21,17 +17,13 @@ resource "local_file" "cloud_init_user_data_file" {
     hostname = var.hostname
     fqdn     = "${var.hostname}.${var.domain_name}"
   })
-  filename = "${var.ci_conf_path}/${local.ci_file_user_filename}.rendered"
 }
 
-resource "null_resource" "cloud_init_config_files" {
-
-  triggers = {
-    ci_file_id = local_file.cloud_init_user_data_file.id
-  }
+resource "terraform_data" "ci_user_file" {
+  triggers_replace = local.user_file_data
 
   provisioner "file" {
-    source      = local_file.cloud_init_user_data_file.filename
+    content     = local.user_file_data
     destination = "/${local.ci_file_user_filename}" # Non-standard path. Read note below
   }
 
--- versions.tf
+++ versions.tf
@@ -6,13 +6,5 @@ terraform {
       source  = "telmate/proxmox"
       version = "~> 2.9.13"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2.1"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.3.0"
-    }
   }
 }
